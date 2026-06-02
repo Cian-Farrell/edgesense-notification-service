f60e6a14f4d3e16ab3fbe8bf7d58f3276beaef45
Cian-Farrell <cian.farrell41@gmail.com>
2026-06-02 10:12:01 +0100

    Add notification service with AWS SNS anomaly alerting

---
+++ src/main/java/com/edgesense/notification/NotificationService.java
@@ -0,0 +1,36 @@
+package com.edgesense.notification;
+
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+import software.amazon.awssdk.services.sns.model.PublishResponse;
+
+@Service
+public class NotificationService {
+
+    private final SnsClient snsClient;
+    private static final String TOPIC_ARN = "arn:aws:sns:eu-west-1:302388404494:edgesense-anomaly-alerts";
+
+    public NotificationService(){
+        this.snsClient = SnsClient.builder()
+                .region(Region.EU_WEST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    public void sendAnomalyAlert(double temp, double humidity, double timeStamp) {
+        String message = String.format("ANOMALY DETECTED!\nTemperature: %.2f°C\nHumidity: %.2f%%\nTimestamp: %.0f",
+                temp, humidity, timeStamp);
+
+        PublishRequest request = PublishRequest.builder()
+                .topicArn(TOPIC_ARN)
+                .message(message)
+                .subject("EdgeSense Anomaly Alert")
+                .build();
+
+        PublishResponse response = snsClient.publish(request);
+        System.out.println("Alert sent with message ID: " + response.messageId());
+    }
+}
